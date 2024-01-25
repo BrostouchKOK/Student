1a193d769a051ce7bb0c094ae297f6f7ce71705f
BrostouchKOK <brostouchkokit1007@gmail.com>
2024-01-25 20:02:05 +0700

    Student

---
+++ src/Student.java
@@ -0,0 +1,10 @@
+public class Student extends Person{
+    String numCourse;
+    String course;
+    String grade;
+
+    public Student(String name, String address) {
+        super(name, address);
+    }
+
+}
